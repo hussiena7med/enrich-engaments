dfa94fe5b2c3659f72d4ec4f32e83a8b29b4533f
hussiena7med <44244633+hussiena7med@users.noreply.github.com>
2021-04-19 01:23:04 +0200

    Update index.js

--- modules/interactions-queue/index.js
+++ modules/interactions-queue/index.js
@@ -1,8 +1,7 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-    accessKeyId: 'AKIA3ADVKLRLGUV2FFDS',
-    secretAccessKey: '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',
+
     region: 'us-east-2'
 });
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
